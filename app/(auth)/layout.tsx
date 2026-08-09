d2c64bcc05e4d8c981d08153bb20d9d86b32821a
saikrishna8076 <saikrishna807666@gmail.com>
2026-08-10 02:43:44 +0530

    setup app,file and folder structure ,routing and auth pages

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Authlayout = ({children} : {children : React.ReactNode}) => {
+  return (
+    <div className='auth-layout'>
+      { children }
+    </div>
+  )
+}
+
+export default Authlayout
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Rootlayout = ({children} : {children : React.ReactNode}) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default Rootlayout
